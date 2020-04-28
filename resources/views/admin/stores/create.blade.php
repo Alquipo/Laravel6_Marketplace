d60edb91fd70384097229a3c3466f0830a17497a
Alquipo Neto <alquiponeto@outlook.com.br>
2020-04-28 16:01:21 -0300

    validação com form request

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,36 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>criar Loja</h1>
+<h1>Criar Loja</h1>
 <form action="{{route('admin.stores.store')}}" method="post">
     @csrf
     <div class="form-group">
-        <label>Loja</label>
-        <input type="text" name="name" id="" class="form-control">
-        
+        <label>Nome Loja</label>
+    <input type="text" name="name" id="" class="form-control  @error('name') is-invalid @enderror" value="{{old('name')}}">
+       
+        @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
     
     <div class="form-group">
         <label>Descricao</label>
-        <input type="text" name="description" id="" class="form-control">
+        <input type="text" name="description" id="" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+
+        @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
         
     </div>
     
     <div class="form-group">
         <label>Telefone</label>
-        <input type="text" name="phone" id="" class="form-control">
+        <input type="text" name="phone" id="" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
         
+        @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
     
     <div class="form-group">
         <label>Celular/whats</label>
-        <input type="text" name="mobile_phone" id="" class="form-control">
+        <input type="text" name="mobile_phone" id="" class="form-control @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
         
+        @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+        @enderror
     </div>
 
     <div>
         <label>slug</label>
-        <input type="text" name="slug" id="" class="form-control">
+        <input type="text" name="slug" id="" class="form-control ">
         
     </div>
 
